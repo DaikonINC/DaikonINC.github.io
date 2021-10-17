e78880fdf06b4d3f05a17480c9d90d7768ca10d0
Michael O'Sullivan <mosul006@uottawa.ca>
2021-10-17 04:35:01 -0400

    Adding goals page with goals page mockup

---
+++ daikon_goals.js
@@ -0,0 +1,14 @@
+/*Showing people for bottom animation*/
+function show(){
+    document.getElementsByClassName('person')[0].style.animation = "community 4s linear";
+    document.getElementsByClassName('person')[0].style.animationFillMode = "forwards";
+    document.getElementsByClassName('person')[1].style.animation = "community 4s linear";
+    document.getElementsByClassName('person')[1].style.animationFillMode = "forwards";
+    document.getElementsByClassName('person')[2].style.animation = "community 4s linear";
+    document.getElementsByClassName('person')[2].style.animationFillMode = "forwards";
+
+    document.getElementsByClassName('line')[0].style.animation = "community 4s linear";
+    document.getElementsByClassName('line')[0].style.animationFillMode = "forwards";
+    document.getElementsByClassName('line')[1].style.animation = "community 4s linear";
+    document.getElementsByClassName('line')[1].style.animationFillMode = "forwards";
+}
