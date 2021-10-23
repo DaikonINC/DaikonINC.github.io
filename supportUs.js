14cfd8847a58d7ae0825e4b6e63eae64fc6963de
Jeremy Dufour <jeremy.k.dufour@gmail.com>
2021-10-23 18:14:50 -0400

    css and js progress

--- supportUs.js
+++ supportUs.js
@@ -1,8 +1,10 @@
 var counter;
 var modal;
+var previousCount;
 window.onload = function(){
     counter = document.getElementById('counter');
-    modal = document.getElementById('donationSignUp')
+    modal = document.getElementById('donationSignUp');
+    previousCount = getCurCount()-1;
     updateCount();
     loopTimer();
     
@@ -14,16 +16,31 @@ function loopTimer() {
     }, 5000);
 }
 
-function updateCount() {
-
+function getCurCount(){
     var date = new Date();
     var time = date.getTime() / 1000;
-
-
-    var downloads = Math.ceil((1.5*time + Math.abs(Math.sin(time)))) - 2446000000
-    counter.innerHTML = "As of " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + ", we have reached over " + downloads.toLocaleString() + " Downloads!";
+    //always increasing function of time ensures that download numbers are consistent across page reloads and that the nummber steadily increases.
+    var downloads = Math.ceil((1.5*time + Math.abs(Math.sin(time)))) - 2446000000;
+    return downloads;
 }
-
+function display(num){
+    var date = new Date();
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+    var curMonth = months[date.getMonth()];
+    counter.innerHTML = "As of " + curMonth + " " + date.getDate() + ", " + date.getFullYear() + ", we have reached over " + num.toLocaleString() + " Downloads!";
+}
+function updateCount() {
+    
+    curCount = getCurCount();
+    console.log("A: "+ (curCount-previousCount))
+    for(let i = 0; i < (curCount-previousCount); i++){      
+        let x = curCount + i;
+        setTimeout(display,i*100,x);
+        console.log("B: " + curCount)
+        
+    }
+    previousCount = curCount;
+}
 function openModal() {
     modal.style.display = "block";
 }
