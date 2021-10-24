011288a083943dbf30a1a32343192d20b0d41474
Jeremy Dufour <jeremy.k.dufour@gmail.com>
2021-10-24 14:30:14 -0400

    supportUS.html now uses daikon_nav.css for navbar

--- supportUs.js
+++ supportUs.js
@@ -66,11 +66,10 @@ function updatePosition() {
     ctx.fillStyle = "rgb(0,100,5)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < leaves.length; i++) {
-        // if out of bounds, go back up
-
         leaves[i][2] += 1
         var x = canvas.width / 2 + canvas.width / 2 * Math.sin(leaves[i][0]-leaves[i][2] * Math.PI / 540);
         var y = leaves[i][1]+ (leaves[i][2]/2)*(leaves[i][3]/50 +0.5)
+         // if out of bounds, go back up
         if(y >= canvas.height){
             leaves[i][0] = Math.floor(Math.random() * canvas.width);
             leaves[i][1] = - img.height;
