1258765696871d6cb6fbcc0135cdc098e2f41606
Jeremy Dufour <jeremy.k.dufour@gmail.com>
2021-11-14 12:35:04 -0500

    updated navbar, added charity links

--- supportUs.js
+++ supportUs.js
@@ -11,12 +11,6 @@ window.onload = function () {
     modal = document.getElementById('donationSignUp');
     previousCount = getCurCount() - 1;
     img = new Image(300, 300);
-    const hamburger = document.querySelector(".hamburger");
-    const mobile_menu = document.querySelector(".mobile");
-    hamburger.addEventListener("click", function(){
-    hamburger.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
-    });
     img.src = 'leafSmall.png';
     updateCount();
     loopTimer();
