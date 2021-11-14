2b272c6e84c2329ef5e99ffa6657a92a0d3eeab6
kyro105360 <61020864+kyro105360@users.noreply.github.com>
2021-11-14 00:29:13 -0500

    reworked home page

---
+++ insta.js
@@ -0,0 +1,7 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJWS3lXNWNvNTFUUmUzcFpQQ0ttenI2Mk53WWFGeWdsalFKdWhVNkRQRV9QcnFxaGRjdWRwSmMyQm90b2hjVFBpQzlMX0ZAYUjVDQXMwb201c0dLMF9DZADJDVjJ2QmQtQVFGRHUxSzk3N3pqdE8xUgZDZD'
+});
+userFeed.run();
